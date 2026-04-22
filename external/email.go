d82b4841830b1538ceb4a792ce5e43bfd23ba467
CakraSera <rakelsera2001@gmail.com>
2026-04-22 08:07:47 +0700

    fix: setup config email

--- external/email.go
+++ external/email.go
@@ -1,14 +1,39 @@
 package external
 
-// import "gopkg.in/gomail.v2"
+import (
+	"strconv"
 
-type External struct{}
+	"ewallet-notification/helper"
 
-func (*External) SendEmail() error {
-	// mailer := gomail.NewMessage()
-	// mailer.SetHeader("To")
-	// mailer.SetHeader("From")
-	// mailer.SetHeader("Subject")
-	// mailer.SetBody("text/html", "Test Body")
+	"gopkg.in/gomail.v2"
+)
+
+type Email struct {
+	To      string
+	From    string
+	Subject string
+	Body    string
+}
+
+func (e *Email) SendEmail() error {
+	mailer := gomail.NewMessage()
+	mailer.SetHeader("To", e.To)
+	mailer.SetHeader("From", e.From)
+	mailer.SetHeader("Subject", e.Subject)
+	mailer.SetBody("text/html", e.Body)
+
+	smtpPort := helper.GetEnv("SMTP_PORT", "")
+	intSmptPort, _ := strconv.Atoi(smtpPort)
+	dialer := gomail.NewDialer(
+		helper.GetEnv("SMTP_HOST", ""),
+		intSmptPort,
+		helper.GetEnv("SMTP_AUTH_EMAIL", ""),
+		helper.GetEnv("SMTP_AUTH_PASSWORD", ""),
+	)
+
+	err := dialer.DialAndSend(mailer)
+	if err != nil {
+		return err
+	}
 	return nil
 }
